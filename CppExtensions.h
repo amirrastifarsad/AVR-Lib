2db074802362a7e67f4498bce809e5f59f72c36b
amirrastifarsad <31597075+amirrastifarsad@users.noreply.github.com>
2019-05-16 20:26:41 +0430

    Update CppExtensions.h

--- CppExtensions.h
+++ CppExtensions.h
@@ -9,14 +9,18 @@
 #ifndef CPPEXTENSIONS_H_
 #define CPPEXTENSIONS_H_
 
+#include <stdlib.h>
 void* operator new (size_t obj){
+	return malloc(obj);
+}
+void* operator new[] (size_t obj){
 	return malloc(obj);
 }
 void operator delete (void* obj){
 	free(obj);
 }
-
-
-
+void operator delete[] (void* obj){
+	free(obj);
+}
 
 #endif /* CPPEXTENSIONS_H_ */
